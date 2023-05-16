5760549bbafa31c83ce7594f1966230dc6bcf7ca
Ludson Pereira <ludson_ps27@hotmail.com>
2023-05-16 14:46:55 -0300

    test: :white_check_mark: add teste para classe Ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,19 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (
+    Ingredient,
+    Restriction,
+)  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    ...
+    farinha1 = Ingredient("farinha")
+    farinha2 = Ingredient("farinha")
+    presunto = Ingredient("presunto")
+
+    assert farinha1.name == "farinha"
+    assert farinha1.restrictions == {Restriction.GLUTEN}
+    assert str(farinha1) == "Ingredient('farinha')"  # __repr__
+    assert farinha1 == farinha2  # __eq__
+    assert farinha1 != presunto  # __eq__
+    assert hash(farinha1) == hash(farinha2)  # __hash__
+    assert hash(farinha1) != hash(presunto)  # __hash__
